1305cfc582f6f44bc879dabdf032d2a3fe6231d2
sonicboom989 <sonicboom989@comcast.net>
2025-04-09 16:12:52 -0500

    Color Filter Upload

---
+++ KUbesat color filter/main.py
@@ -0,0 +1,9 @@
+from filtercolor import *
+
+def main():
+    filaname = 'KUbesat color filter\kubesattestpic.png'
+    lower_green = np.array([35, 40, 40])   # Lower bound of green in HSV
+    upper_green = np.array([85, 255, 255]) # Upper bound of green in HSV
+
+    def filter_color(filename, lower_green, upper_green)
+main()
