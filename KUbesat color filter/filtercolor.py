322cc8b0b976f213e080ffc0c354b8012c0af68f
Max Georges <max.georges465@gmail.com>
2025-11-20 17:39:27 -0600

    two banded filter

--- KUbesat color filter/filtercolor.py
+++ KUbesat color filter/filtercolor.py
@@ -29,8 +29,8 @@ def filter_color(image_path, lower_bound, upper_bound, output_path=None):
 
 def main():
     filename = 'kubesattestpic.png'
-    lower_green = np.array([35, 40, 40])   # Lower bound of green in HSV
-    upper_green = np.array([85, 255, 255]) # Upper bound of green in HSV
+    lower_green = np.array([0, 0, 0])   # Lower bound of green in HSV
+    upper_green = np.array([255, 255, 255]) # Upper bound of green in HSV
 
     filter_color(filename, lower_green, upper_green)
 
--- KUbesat color filter/main.py
+++ KUbesat color filter/main.py
@@ -1,9 +1,14 @@
 from filtercolor import *
 
 def main():
-    filaname = 'KUbesat color filter\kubesattestpic.png'
-    lower_green = np.array([35, 40, 40])   # Lower bound of green in HSV
-    upper_green = np.array([85, 255, 255]) # Upper bound of green in HSV
+    filaname = 'KUbesat color filter\kubesattest2.png'
+    lower_1 = np.array([25, 30, 30])   # Lower bound of color 1 in HSV
+    upper_1 = np.array([95, 255, 255]) # Upper bound of color 1 in HSV
+    lower_2 =  np.array([0, 30, 30]) # Upper bound of color 2 in HSV
+    upper_2 = np.array([15,255,255]) # Upper bound of color 2 in HSV
 
-    def filter_color(filename, lower_green, upper_green)
+
+
+    file1=filter_color(filaname, lower_1, upper_1, output_path='kubesatfilter1.png')
+    file2=filter_color(filaname, lower_2, upper_2, output_path='kubesatfilter2.png')
 main()
